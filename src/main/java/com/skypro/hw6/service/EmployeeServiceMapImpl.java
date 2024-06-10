3675a9dee610321c26333bf286f4b48f36054075
Daria Orlova <da.orlova365@gmail.com>
2024-06-10 20:26:21 +0300

    Initial Commit

--- src/main/java/com/skypro/hw6/service/EmployeeServiceMapImpl.java
+++ src/main/java/com/skypro/hw6/service/EmployeeServiceMapImpl.java
@@ -6,32 +6,31 @@ import com.skypro.hw6.exception.EmployeeStorageIsFullException;
 import com.skypro.hw6.model.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
 public class EmployeeServiceMapImpl implements EmployeeService {
     private static final int NUMBER_OF_EMPLOYEES = 5;
-    private final Map<String,Employee> employees = new HashMap<>(NUMBER_OF_EMPLOYEES);
+    private final Map<String, Employee> employees = new HashMap<>(NUMBER_OF_EMPLOYEES);
+
     @Override
     public Employee add(String firstName, String lastName) {
         if (employees.size() >= NUMBER_OF_EMPLOYEES) {
             throw new EmployeeStorageIsFullException("Массив переполнен");
         }
         Employee employee = new Employee(firstName, lastName);
-        if (employees.containsKey(firstName+lastName)) {
+        if (employees.containsKey(firstName + lastName)) {
             throw new EmployeeAlreadyAddedException("Этот сотрудник уже был добавлен");
         }
-        employees.put(firstName+lastName, employee);
+        employees.put(firstName + lastName, employee);
         return employee;
     }
 
     @Override
     public Employee delete(String firstName, String lastName) {
-        Employee employee = employees.remove(firstName+lastName);
-        if (employee != null){
+        Employee employee = employees.remove(firstName + lastName);
+        if (employee != null) {
             return employee;
         }
         throw new EmployeeNotFoundException(firstName + " " + lastName + " не найден");
@@ -39,8 +38,8 @@ public class EmployeeServiceMapImpl implements EmployeeService {
 
     @Override
     public Employee find(String firstName, String lastName) {
-        Employee employee = employees.get(firstName+ lastName);
-        if (employee!=null){
+        Employee employee = employees.get(firstName + lastName);
+        if (employee != null) {
             return employee;
         }
         throw new EmployeeNotFoundException(firstName + " " + lastName + " не найден");
